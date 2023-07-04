96c10dc3d3b41259d676f184f0a7e4d64d4181b2
David Hollenbeck <david@ucoastweb.com>
2023-07-04 03:20:15 -0700

    variant-radios

--- src/scripts/theme/variant-radios.ts
+++ src/scripts/theme/variant-radios.ts
@@ -1,11 +1,12 @@
 import { VariantSelects } from '@/scripts/theme/variant-selects';
+import { qsaOptional } from '@/scripts/functions';
 
 export class VariantRadios extends VariantSelects {
   constructor() {
     super();
   }
 
-  override setInputAvailability(listOfOptions, listOfAvailableOptions) {
+  override setInputAvailability(listOfOptions:(HTMLInputElement|HTMLOptionElement)[], listOfAvailableOptions:(string|null)[]) {
     listOfOptions.forEach((input) => {
       if (listOfAvailableOptions.includes(input.getAttribute('value'))) {
         input.classList.remove('disabled');
@@ -16,9 +17,16 @@ export class VariantRadios extends VariantSelects {
   }
 
   override updateOptions() {
-    const fieldsets = Array.from(this.querySelectorAll('fieldset'));
+    const fieldsetNodes = qsaOptional<HTMLFieldSetElement>('fieldset', this);
+    if (!fieldsetNodes) throw new Error('cannot do updateOptions in variant-radios, no fieldset nodes found');
+    const fieldsets = Array.from(fieldsetNodes);
     this.options = fieldsets.map((fieldset) => {
-      return Array.from(fieldset.querySelectorAll('input')).find((radio) => radio.checked).value;
+      const inputNodes = qsaOptional<HTMLInputElement>('input', fieldset);
+      if (!inputNodes) throw new Error('cannot do updateOptions in variant-radios, no input nodes found');
+      const inputs = Array.from(inputNodes);
+      const checkedInput = inputs.find((radio) => radio.checked)
+      if (!checkedInput) throw new Error('cannot do updateOptions in variant-radios, no checked input found');
+      return checkedInput.value;
     });
   }
 }
