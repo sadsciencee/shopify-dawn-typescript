562c514432303dd19d43380ce3502e88eaeb2f7d
David Hollenbeck <david@ucoastweb.com>
2023-07-09 12:09:52 -0700

    price-range

--- src/scripts/catalog/price-range.ts
+++ src/scripts/catalog/price-range.ts
@@ -1,39 +1,52 @@
-import { UcoastEl } from '@/scripts/core/UcoastEl';
-import { currentTargetRequired, getAttributeOrThrow } from '@/scripts/functions';
+import { UcoastEl } from '@/scripts/core/UcoastEl'
+import {
+	currentTargetRequired,
+	getAttributeOrThrow,
+	qsaRequired,
+	qsRequired,
+} from '@/scripts/functions'
 
 export class PriceRange extends UcoastEl {
-  static htmlSelector = 'price-range'
-  constructor() {
-    super()
-    this.querySelectorAll('input').forEach((element) =>
-      element.addEventListener('change', this.onRangeChange.bind(this))
-    )
-    this.setMinAndMaxValues()
-  }
+	static htmlSelector = 'price-range'
+	static selectors = {
+		input: '[data-uc-price-range-input]',
+		max: '[data-uc-price-range-input="max"]',
+		min: '[data-uc-price-range-input="min"]',
+	}
+	inputs: NodeListOf<HTMLInputElement>
+	maxInput: HTMLInputElement
+	minInput: HTMLInputElement
+	constructor() {
+		super()
+		this.maxInput = qsRequired(PriceRange.selectors.max, this)
+		this.minInput = qsRequired(PriceRange.selectors.min, this)
+		this.inputs = qsaRequired(PriceRange.selectors.input, this)
+		this.inputs.forEach((element) =>
+			element.addEventListener('change', this.onRangeChange.bind(this))
+		)
+		this.setMinAndMaxValues()
+	}
 
-  onRangeChange(event: Event) {
-    const currentTarget = currentTargetRequired<Event, HTMLInputElement>(event)
-    this.adjustToValidValues(currentTarget)
-    this.setMinAndMaxValues()
-  }
+	onRangeChange(event: Event) {
+		const currentTarget = currentTargetRequired<Event, HTMLInputElement>(event)
+		this.adjustToValidValues(currentTarget)
+		this.setMinAndMaxValues()
+	}
 
-  setMinAndMaxValues() {
-    const inputs = this.querySelectorAll('input')
-    const minInput = inputs[0]
-    const maxInput = inputs[1]
-    if (maxInput.value) minInput.setAttribute('max', maxInput.value)
-    if (minInput.value) maxInput.setAttribute('min', minInput.value)
-    if (minInput.value === '') maxInput.setAttribute('min', '0')
-    if (maxInput.value === '')
-      minInput.setAttribute('max', getAttributeOrThrow('max', maxInput))
-  }
+	setMinAndMaxValues() {
+		if (this.maxInput.value) this.minInput.setAttribute('max', this.maxInput.value)
+		if (this.minInput.value) this.maxInput.setAttribute('min', this.minInput.value)
+		if (this.minInput.value === '') this.maxInput.setAttribute('min', '0')
+		if (this.maxInput.value === '')
+			this.minInput.setAttribute('max', getAttributeOrThrow('max', this.maxInput))
+	}
 
-  adjustToValidValues(input: HTMLInputElement) {
-    const value = Number(input.value)
-    const min = Number(getAttributeOrThrow('min', input))
-    const max = Number(getAttributeOrThrow('max', input))
+	adjustToValidValues(input: HTMLInputElement) {
+		const value = Number(input.value)
+		const min = Number(getAttributeOrThrow('min', input))
+		const max = Number(getAttributeOrThrow('max', input))
 
-    if (value < min) input.value = `${min}`
-    if (value > max) input.value = `${max}`
-  }
+		if (value < min) input.value = `${min}`
+		if (value > max) input.value = `${max}`
+	}
 }
